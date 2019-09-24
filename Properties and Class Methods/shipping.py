3c09e5f575a0f665f2a9cf55aa2f599bd21c0514
Angus <anguscburns@gmail.com>
2019-09-24 14:20:33 -0600

    Properties and Class Methods after Duck Tail

--- Properties and Class Methods/shipping.py
+++ Properties and Class Methods/shipping.py
@@ -36,6 +36,9 @@ class ShippingContainer:
 
     @property
     def volume_ft3(self):
+        return self._calc_volume()
+
+    def _calc_volume(self):
         return ShippingContainer.HEIGHT_FT * ShippingContainer.WIDTH_FT * self.length_ft
 
 
@@ -69,6 +72,9 @@ class RefrigeratedShippingContainer(ShippingContainer):
 
     @celsius.setter
     def celsius(self, value):
+        self._set_celsius(value)
+
+    def _set_celsius(self, value):
         if value > RefrigeratedShippingContainer.MAX_CELSIUS:
             raise ValueError("Temperature too hot!")
         self._celsius = value
@@ -81,17 +87,15 @@ class RefrigeratedShippingContainer(ShippingContainer):
     def fahrenheit(self, value):
         self._celsius = RefrigeratedShippingContainer._f_to_c(value)
 
-    @property
-    def volume_ft3(self):
-        return super().volume_ft3 - RefrigeratedShippingContainer.FRIDGE_VOLUME_FT3
+    def _calc_volume(self):
+        return super()._calc_volume() - RefrigeratedShippingContainer.FRIDGE_VOLUME_FT3
 
 
 class HeatedRefrigeratedShippingContainer(RefrigeratedShippingContainer):
 
     MIN_CELSIUS = -20.0
 
-    @RefrigeratedShippingContainer.celsius.setter
-    def celsius(self, value):
+    def _set_celsius(self, value):
         if value < HeatedRefrigeratedShippingContainer.MIN_CELSIUS:
             raise ValueError("Temperature out of range!")
-        RefrigeratedShippingContainer.celsius.fset(self, value)
+        super()._set_celsius(value)
